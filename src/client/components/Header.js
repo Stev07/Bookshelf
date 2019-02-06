90c4ae5ca9bf06cdc1077ec229c58ec8765b47d9
domreka <dominiquebosman02@gmail.com>
2019-02-06 13:58:58 +0100

    pull commit

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,19 +1,29 @@
 import * as React from "react";
+import logoBecode from "../images/logo_Becode.png";
 
 export default class Header extends React.Component {
     constructor() {
         super();
         this.state = {
-            logo: "",
+            logo: logoBecode,
             headerTitle: "<BookShelf />",
+            user: "Hello ",
         };
-    }
+    } // Données utilisées pour le header
 
     render() {
         return (
-            <div>
-                <img src="" />
-                <h1>{this.state.headerTitle}</h1>
+            <div className="header">
+                <div className="logoBox">
+                    <img
+                        className="logo"
+                        src={this.state.logo}
+                        alt="BeCode.org"
+                    />
+                </div>
+                <div className="titleBox">
+                    <h1 className="title">{this.state.headerTitle}</h1>
+                </div>
             </div>
         );
     }
