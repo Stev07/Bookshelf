2cf2ebfa848866bcd702aeae1a634c3aaebaf935
Steve Dossin <steve.dossin@gmail.com>
2019-02-06 13:54:38 +0100

    component login

---
+++ src/client/components/Login.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./scss/login.scss";
+
+class Login extends React.Component {
+    render() {
+        return <div className="container" />;
+    }
+}
+
+export default Login;
