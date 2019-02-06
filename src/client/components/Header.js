0442bc88703c15663f2224e16b86a3463dd165dc
domreka <dominiquebosman02@gmail.com>
2019-02-06 15:12:58 +0100

    add link logo

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import logoBecode from "../images/logo_Becode.png";
+import "./scss/AppDom.scss";
+import Home from "./Home";
 
 export default class Header extends React.Component {
     constructor() {
@@ -8,6 +10,7 @@ export default class Header extends React.Component {
             logo: logoBecode,
             headerTitle: "<BookShelf />",
             user: "Hello ",
+            homeLink: Home,
         };
     } // Données utilisées pour le header
 
@@ -15,10 +18,18 @@ export default class Header extends React.Component {
         return (
             <div className="header">
                 <div className="logoBox">
-                    <img
-                        className="logo"
-                        src={this.state.logo}
-                        alt="BeCode.org"
+                    <a href="./Home.js">
+                        <img
+                            className="logo"
+                            src={this.state.logo}
+                            alt="BeCode.org"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <input
+                        type="search"
+                        placeholder="Looking for a book or an author ?"
                     />
                 </div>
                 <div className="titleBox">
