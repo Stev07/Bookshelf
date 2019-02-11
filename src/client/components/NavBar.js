37108555dfed8fb78e9c2dc46c62e227df81a434
Steve Dossin <steve.dossin@gmail.com>
2019-02-11 18:17:12 +0100

    commit before merge

--- src/client/components/NavBar.js
+++ src/client/components/NavBar.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
+import "./scss/navbar.scss";
 
 export default class NavBar extends React.Component {
     render() {
         return (
-            <div>
+            <div className="navbar">
                 <Link to="/home"> {"Home"} </Link>
                 <Link to="/books"> {"Books"} </Link>
                 <Link to="/author"> {"Author"} </Link>
