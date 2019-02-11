8e281f8885e51cc652e5510447d6f4d16af6e98b
domreka <dominiquebosman02@gmail.com>
2019-02-11 16:06:42 +0100

    create showbooks component and beginning the homepage css and data showing

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -9,25 +9,47 @@
 import * as React from "react";
 import Header from "./Header";
 import NavBar from "./NavBar";
+import "./scss/AppDom.scss";
+import Cover from "../images/ReactJSEssentials.png"
 
 export default class Home extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            cover: Cover,
+        }
+    }
+
     render() {
         return (
             <div>
                 <Header />
                 <NavBar />
 
-                <div>
-                    <h1>{"<Bookshelf />"}</h1>
-                </div>
                 <div className="section">
                     <p className="sectionName">{"Latest entries"}</p>
+                </div>
+                <div className="showSection">
+                    <div className="bookCover">
+                        <img
+                            className="book"
+                            src={this.state.cover}
+                            alt="book"
+                        />
+                    </div>
                 </div>
                 <div className="section">
                     <p className="sectionName">{"Most rated"}</p>
                 </div>
+                <div className="showSection">
+
+                </div>
                 <div className="section">
                     <p className="sectionName">{"Latest Review"}</p>
+                </div>
+                <div className="showSection">
+
                 </div>
             </div>
         );
---
+++ src/client/components/ShowBooks.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const books =  book [
+                title = '',
+                author = '',
+                releaseDate = '',
+                bookCover = '',
+                language = '',
+                description = ''
+    ]
+
+
+
+export default class ShowBooks extends React.Component {
+    
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
