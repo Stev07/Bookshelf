5c71ec378e0002c017327489bc78c7f5bf52dc5e
Steve Dossin <steve.dossin@gmail.com>
2019-02-12 14:11:59 +0100

    navbar + some css

--- src/client/components/NavBar.js
+++ src/client/components/NavBar.js
@@ -3,13 +3,49 @@ import {Link} from "react-router-dom";
 import "./scss/navbar.scss";
 
 export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    handleScroll() {
+        this.setState({scroll: window.scrollY});
+    }
+
+    componentDidMount() {
+        const el = document.querySelector("nav");
+
+        this.setState({top: el.offsetTop, height: el.offsetHeight});
+        window.addEventListener("scroll", this.handleScroll);
+    }
+
+    componentDidUpdate() {
+        this.state.scroll > this.state.top
+            ? (document.body.style.paddingTop = `${this.state.height}px`)
+            : (document.body.style.paddingTop = 0);
+    }
+
     render() {
         return (
-            <div className="navbar">
-                <Link to="/home"> {"Home"} </Link>
-                <Link to="/books"> {"Books"} </Link>
-                <Link to="/author"> {"Author"} </Link>
-            </div>
+            <nav
+                className={
+                    this.state.scroll > this.state.top ? "fixed-nav" : ""
+                }>
+                <div className="navLinks">
+                    <li className="navItem">
+                        <Link to="/home"> {"Home"} </Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to="/books"> {"Books"} </Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to="/author"> {"Author"} </Link>
+                    </li>
+                </div>
+            </nav>
         );
     }
 }
