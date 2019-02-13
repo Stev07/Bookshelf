bc838f84a830711d393df9c210bf26b3a9576bd4
Steve Dossin <steve.dossin@gmail.com>
2019-02-13 15:50:46 +0100

    commit before merge dev

--- src/client/components/ShowBooks.js
+++ src/client/components/ShowBooks.js
@@ -1,6 +1,7 @@
 import React from "react";
 import data from "./data.json";
 import "./scss/AppDom.scss";
+import {Modal} from "antd";
 
 export default class ShowBooks extends React.Component {
     constructor() {
@@ -8,10 +9,11 @@ export default class ShowBooks extends React.Component {
 
         this.state = {
             data: data,
+            visible: false,
         };
 
         this.books = this.state.data.map(book => (
-            <div>
+            <div key={book.id}>
                 <div
                     className="bookContainer"
                     style={{background: "white", padding: "30px"}}>
@@ -24,6 +26,7 @@ export default class ShowBooks extends React.Component {
                     </div>
                     <div className="cardCover">
                         <img
+                            onClick={this.showModal}
                             className="bookCover"
                             src={book.cover}
                             alt={book.title}
@@ -39,10 +42,30 @@ export default class ShowBooks extends React.Component {
         ));
     }
 
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    handleCancel = () => {
+        this.setState({
+            visible: false,
+        });
+    };
+
     render() {
         return (
             <div className="dbContent">
                 <div className="card-container">{this.books}</div>
+                <Modal
+                    title="Basic Modal"
+                    visible={this.state.visible}
+                    onCancel={this.handleCancel}>
+                    <p>{"Some contents..."}</p>
+                    <p>{"Some contents..."}</p>
+                    <p>{"Some contents..."}</p>
+                </Modal>
             </div>
         );
     }
