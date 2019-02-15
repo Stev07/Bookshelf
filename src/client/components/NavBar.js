0d569f2187977d8b7569fef689a058a3ea1f18c0
Steve Dossin <steve.dossin@gmail.com>
2019-02-15 16:11:07 +0100

    home responsive

--- src/client/components/NavBar.js
+++ src/client/components/NavBar.js
@@ -39,11 +39,11 @@ export default class NavBar extends React.Component {
                     <li className="navItem">
                         <Link to="/home"> {"Home"} </Link>
                     </li>
-                    <div>{"|"}</div>
+                    <div className="separate">{"|"}</div>
                     <li className="navItem">
                         <Link to="/books"> {"Books"} </Link>
                     </li>
-                    <div>{"|"}</div>
+                    <div className="separate">{"|"}</div>
                     <li className="navItem">
                         <Link to="/author"> {"Author"} </Link>
                     </li>
